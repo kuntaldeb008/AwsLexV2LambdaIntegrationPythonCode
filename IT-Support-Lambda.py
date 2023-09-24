80cc6a9d5bab2305cf8484a57ecef7a414b16723
kuntaldeb008 <40393222+kuntaldeb008@users.noreply.github.com>
2023-09-24 19:09:26 +0530

    AWS Lambda Python Code Inetgrated with AWS_Lex_V2_Bot_Service for validation and creating ticket for IT related Issue

---
+++ IT-Support-Lambda.py
@@ -0,0 +1,114 @@
+import json
+import random
+
+def validate(slots):
+    
+    if not slots['Id']:
+        print("Inside Empty Id")
+        return {
+        'isValid': False,
+        'violatedSlot': 'Id'
+        }        
+        
+    if not slots['Application']:
+        print("Inside Empty Application")
+        return {
+        'isValid': False,
+        'violatedSlot': 'Application',
+    }
+
+    return {'isValid': True}
+    
+    
+def lambda_handler(event, context):
+    # TODO implement
+    print(event)
+    
+    slots = event['sessionState']['intent']['slots']
+    intent = event['sessionState']['intent']['name']
+    print(event['invocationSource'])
+    print(slots)
+    print(intent)
+    validation_result = validate(event['sessionState']['intent']['slots'])
+    
+    if event['invocationSource'] == 'DialogCodeHook':
+        if not validation_result['isValid']:
+            
+            if 'message' in validation_result:
+            
+                response = {
+                "sessionState": {
+                    "dialogAction": {
+                        'slotToElicit':validation_result['violatedSlot'],
+                        "type": "ElicitSlot"
+                    },
+                    "intent": {
+                        'name':intent,
+                        'slots': slots
+                        
+                        }
+                },
+                "messages": [
+                    {
+                        "contentType": "PlainText",
+                        "content": validation_result['message']
+                    }
+                ]
+               } 
+            else:
+                response = {
+                "sessionState": {
+                    "dialogAction": {
+                        'slotToElicit':validation_result['violatedSlot'],
+                        "type": "ElicitSlot"
+                    },
+                    "intent": {
+                        'name':intent,
+                        'slots': slots
+                        
+                        }
+                }
+               } 
+    
+            return response
+           
+        else:
+            response = {
+            "sessionState": {
+                "dialogAction": {
+                    "type": "Delegate"
+                },
+                "intent": {
+                    'name':intent,
+                    'slots': slots
+                    
+                    }
+        
+            }
+        }
+            return response
+    
+    if event['invocationSource'] == 'FulfillmentCodeHook':
+        
+        response = {
+        "sessionState": {
+            "dialogAction": {
+                "type": "Close"
+            },
+            "intent": {
+                'name':intent,
+                'slots': slots,
+                'state':'Fulfilled'
+                
+                }
+    
+        },
+        "messages": [
+            {
+                "contentType": "PlainText",
+                "content": "This is your Ticket Number : INC-" + str(random.randint(1000000, 9999999)) + " , please keep it for your reference."
+            }
+        ]
+    }
+            
+        return response
